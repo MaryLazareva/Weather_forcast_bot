b99fe6286d1f0c37d83d5e055c9e469529c7d16f
mary lazareva <lazareva.m1989@yandex.ru>
2025-01-11 23:22:18 +0300

    Changing alembic file

--- alembic/env.py
+++ alembic/env.py
@@ -1,5 +1,5 @@
 from logging.config import fileConfig
-
+import os
 import asyncio
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
@@ -7,12 +7,20 @@ from sqlalchemy.ext.asyncio import create_async_engine
 from database import Base
 from models import CityForecast, ForecastDetails, WeatherIcons
 
+from dotenv import load_dotenv
 
 from alembic import context
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+
+if DATABASE_URL:
+    config.set_main_option("sqlalchemy.url", DATABASE_URL)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
